5d90df14e66c490f4afb6cdea531edf7ec21e33b
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-08-31 22:09:26 +0300

    correct yandex

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
   creator: "Ahmad Falta",
   verification: {
     google: "ABZQt2rUsxl9LrTiWV7YqdMdPJTI70HYtPLS0z385ts",
-    yandex: "91fc491a87c3a7fe",
+    yandex: "08bd74f1be0753d1",
   },
 };
 
