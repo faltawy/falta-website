b547b702eac5089f4fe2d1534a6a0c48ed33b84e
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-01 20:35:59 +0300

    mdx, contentlayer integration for blogs section

---
+++ app/(blog)/blog/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { allBlogs } from "contentlayer/generated";
+import { getMDXComponent } from "next-contentlayer/hooks";
+
+export const generateStaticParams = async () =>
+  allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+  return { title: blog?.title };
+};
+
+// TODO: handle if no blog is found
+const PostLayout = ({ params }: { params: { slug: string } }) => {
+  const blog = allBlogs.find(
+    (blog) => blog._raw.flattenedPath.trim() === params.slug
+  );
+
+  const Content = getMDXComponent(blog?.body.code || "");
+
+  return (
+    <article className="py-8 mx-auto max-w-xl">
+      <div className="mb-8 text-center">
+        <h1>{blog?.title}</h1>
+      </div>
+      <Content />
+    </article>
+  );
+};
+
+export default PostLayout;
