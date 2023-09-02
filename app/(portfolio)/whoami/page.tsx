73fcb26c70a0c6d48f99ff8992fa19fbafb77c54
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-02 12:22:48 +0300

    whoami page

--- app/(portfolio)/whoami/page.tsx
+++ app/(portfolio)/whoami/page.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import Image from "next/image";
 
 function WhoamiPage() {
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-start justify-start h-full w-full flex-col padding-6">
+      <div className="flex-1 w-full mt-4">
+        <section className="w-full flex justify-between items-center gap-10">
+          <div>
+            <h1 className="text-8xl font-bold text-gray uppercase font-inter">
+              about me
+            </h1>
+            <div className="block mt-4">
+              <p className="text-lg font-fira-sans">
+                Welcome to my digital realm! I'm Ahmad Hassan, a self-taught
+                frontend software engineer hailing from the picturesque
+                landscapes of Egypt. My journey into the world of web
+                development started just one year ago, driven by an insatiable
+                curiosity and a desire to transform ideas into digital reality.
+              </p>
+            </div>
+          </div>
+          <div
+            data-container="my-image"
+            className="border-2 border-gray p-2 w-fit h-full"
+          >
+            <div className="relative h-80 aspect-square object-cover grayscale">
+              <Image
+                src="/assets/my-image.jpg"
+                alt="Picture of the author"
+                fill
+              />
+            </div>
+          </div>
+          <div></div>
+        </section>
+      </div>
     </div>
   );
 }
