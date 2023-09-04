8d65637f2505cf5d1f3888ed42b1f2fdd37624c2
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-04 08:45:01 +0300

    added code.falta.com

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const { withContentlayer } = require("next-contentlayer");
 
-const nextConfig = {};
+const nextConfig = {
+  rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "code.falta.com",
+          },
+        ],
+        destination: "https://github.com/ah7255703",
+      },
+    ];
+  },
+};
 
 module.exports = withContentlayer(nextConfig);
